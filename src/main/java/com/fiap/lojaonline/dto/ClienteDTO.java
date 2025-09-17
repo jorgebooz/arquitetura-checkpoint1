798d9b99512b40d13ebab4364f64a88203f48172
jorgebooz <jorg.jhfb@gmail.com>
2025-09-17 18:09:50 -0300

    fix: ClienteDTO and ProdutoDTO
    Corrections from record to class and variable instantiation

--- src/main/java/com/fiap/lojaonline/dto/ClienteDTO.java
+++ src/main/java/com/fiap/lojaonline/dto/ClienteDTO.java
@@ -1,4 +1,25 @@
 package com.fiap.lojaonline.dto;
 
-public record ClienteDTO(String nome, Long id) {
+import com.fiap.lojaonline.model.Cliente;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
+@Getter
+@Setter
+public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String documento;
+
+    public ClienteDTO(){}
+
+    public ClienteDTO(Cliente entity){
+        this.id = entity.getId();
+        this.nome = entity.getNome();
+        this.email = entity.getEmail();
+        this.documento = entity.getDocumento();
+    }
 }
--- src/main/java/com/fiap/lojaonline/dto/ProdutoDTO.java
+++ src/main/java/com/fiap/lojaonline/dto/ProdutoDTO.java
@@ -1,4 +1,28 @@
 package com.fiap.lojaonline.dto;
 
-public record ProdutoDTO(String nome, Long id) {
+import com.fiap.lojaonline.model.Produto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoDTO{
+    private Long id;
+    private String nome;
+    private String preco;
+    private String categoria;
+    private String descricao;
+    private Boolean ativo;
+
+    public ProdutoDTO(){}
+
+    public ProdutoDTO(Produto entity){
+        this.id = getId();
+        this.nome = getNome();
+        this.preco = getPreco();
+        this.categoria = getCategoria();
+        this.descricao = getDescricao();
+        this.ativo = getAtivo();
+    }
+
 }
