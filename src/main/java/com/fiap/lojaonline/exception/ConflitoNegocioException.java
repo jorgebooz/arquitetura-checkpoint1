3cbfd6b8af2d0c528809d2e05946698e845a915f
jorgebooz <jorg.jhfb@gmail.com>
2025-09-18 22:01:55 -0300

    exceptions

---
+++ src/main/java/com/fiap/lojaonline/exception/ConflitoNegocioException.java
@@ -0,0 +1,7 @@
+package com.fiap.lojaonline.exception;
+
+public class ConflitoNegocioException extends RuntimeException {
+    public ConflitoNegocioException(String message) {
+        super(message);
+    }
+}
